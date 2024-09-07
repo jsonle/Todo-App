edd2a04d24a8e451fa99e9866774afed85765c8a
jsonle <jasondle95@gmail.com>
2024-09-07 09:26:26 -0700

    added initial states for todos, useEffect, and fetching api for todos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { supabase } from './supabaseclient';
 import './App.css';
 
 function App() {
+  // State to hold list of todos that are fetched from supabase db
+  const [todos, setTodos] = useState([]);
+  // State to hold new todos being added
+  const [newTodo, setNewtodo] = useState('');
+
+  useEffect(() => {
+    fetchTodos();
+  }, [])
+
+  // Fetch todos from supabase 'todos' table
+  const fetchTodos = async() => {
+    let { data: todos, error } = await supabase
+      .from('todos')
+      .select('*'); // Select all columns
+
+    if (error) {
+      console.log(error);
+    } else {
+      setTodos(todos);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
